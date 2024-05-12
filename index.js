b6e87fd6a6bde6f73c5bb33adcbafc52d56af40d
AyushDhamankar <blockchaindev.ayush@gmail.com>
2024-05-12 13:58:55 +0530

    Port changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use("/api/user", userRoutes);
 // ENV Data
 require("dotenv").config();
 const database = process.env.DATABASE
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 mongoose
   .connect(
